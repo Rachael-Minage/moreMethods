d5aff3b6446e2c06662291cfff169a8650e7e502
Rachael-Minage <lauryneminagge@gmail.com>
2022-06-10 21:40:27 +0300

    balance method

--- bank.py
+++ bank.py
@@ -1,5 +1,8 @@
 # Add these methods to class Account - deposit, withdraw. 
 # Create two instances of account and verify they work as expected.
+from fnmatch import translate
+
+
 class Account:
     def __init__(self,account_number,account_name):
         self.balance = 0
@@ -23,13 +26,26 @@ class Account:
         elif amount<=0:
             return f"You cannot withdraw {amount}"
         else:
-            self.balance-=amount
+            self.transaction_cost = 100
+
+            self.balance-=amount + self.transaction_cost
+            
+
+            # self.last_balance= self.balance-self.transaction_cost
             self.withdrawals.append(amount)
-            return f"You have withdrawn {amount}, your balance is {self.balance}", self.withdrawals
+            return f"You have withdrawn {amount},transaction cost is {self.transaction_cost} your balance is {self.balance}", self.withdrawals
 # Add a new method called deposits_statement which prints each deposit in a new line
     def deposits_statements(self):
-        print(*self.deposits, sep = "\n")  
+        for amount in self.deposits:
+            print(f"You have deposited {amount}")
 #  Add a new method called withdrawals_statement which prints each withdrawal in a new line
     def withdrawals_statement(self):
-     print(*self.withdrawals, sep = "\n" )
+    #  print(*self.withdrawals, sep = "\n" )
+         for amount in self.withdrawals:
+            print(f"You have withdrawn {amount}")
+
+    #  Add a method to show the current balance
+
+    def show_balance(self):
+        print(f"You have withdrawn{self.withdrawals}, transaction cost is {self.transaction_cost}, your balance is {self.balance}")
     
